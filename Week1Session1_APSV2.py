ee59532466e4a8b21b197ff6a53ed07c582a22be
Rajasekhar1131997 <rkolagotla@csus.edu>
2025-06-17 03:24:14 -0700

    TIP102 Sessions

--- Week1Session1_APSV2.py
+++ Week1Session1_APSV2.py
@@ -140,3 +140,58 @@ print(wealthiest_customer(accounts))
 # in the list nums[i], determines the number of other elements in the array that are smaller than it. 
 # More formally, for each nums[i] count the number of valid j's such that j != i and nums[j] < nums[i].
 # Return the answer as a list.
+def smaller_than_current(nums):
+    result = []
+    for i in range(len(nums)):
+        count = 0
+        for j in range(len(nums)):
+            if (j!=i) and (nums[j] < nums[i]):
+                count += 1
+        result.append(count)
+    return result
+
+
+nums = [8, 1, 2, 2, 3]
+print(smaller_than_current(nums))
+
+nums = [6, 5, 4, 8]
+print(smaller_than_current(nums))
+
+nums = [7, 7, 7, 7]
+print(smaller_than_current(nums))
+
+# Problem 7: Diagonal
+# Write a function diagonal_sum() that accepts a 2D n x n matrix grid and returns the sum of the matrix diagonals. 
+# Only include the sum of all the elements on the primary diagonal and all the elements in the secondary diagonal 
+# that are not part of the primary diagonal.
+# The primary diagonal is all cells in the matrix along a line drawn from the top-left cell in the matrix to the bottom-right cell. 
+# The secondary diagonal is all cells in the matrix along a line drawn from the top-right cell in the matrix to the bottom-left cell.
+def diagonal_sum(grid):
+    total = 0
+    n = len(grid)
+    for i in range(n):
+        total = total + grid[i][i]
+        total = total + grid[i][n-1-i]
+    if n % 2 == 1:
+        total = total - grid[n // 2][n // 2]
+    return total
+
+grid = [
+	[1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(diagonal_sum(grid))
+
+grid = [
+	[1, 1, 1, 1],
+    [1, 1, 1, 1],
+	[1, 1, 1, 1],
+    [1, 1, 1, 1]
+]
+print(diagonal_sum(grid))
+
+grid = [
+	[5]
+]
+print(diagonal_sum(grid))
