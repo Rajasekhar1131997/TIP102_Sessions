4909009be7d6be54446948ff48df689bbbbb3bbb
Rajasekhar1131997 <rkolagotla@csus.edu>
2025-06-28 22:59:49 -0700

    TIP102 Sessions

--- Unit_3/Week3Session2_SPSV2.py
+++ Unit_3/Week3Session2_SPSV2.py
@@ -90,9 +90,89 @@ Hint: Try using two variables: one to track the smallest available number and on
 As you process each character in the string, assign the smallest number when the next elevation should increase ('I'), and assign the largest number when the next elevation should decrease ('D').
 """
 def terrain_elevation_match(terrain):
+    return 0
     
 
 print("--------Problem 3---------")
 print(terrain_elevation_match("IDID")) 
 print(terrain_elevation_match("III")) 
 print(terrain_elevation_match("DDI"))
+
+
+"""
+Problem 4: Find the Expedition Log Concatenation Value
+You are recording journal entries during a global expedition, where each entry is represented by a 0-indexed integer array, logs. 
+The concatenation of two journal entries means combining their numerals into one.
+For example, concatenating the numbers 15 and 49 results in 1549.
+Your task is to calculate the total concatenation value of all the journal entries, which starts at 0. 
+To do this, perform the following steps until no entries remain:
+1. If there are at least two entries in the logs, concatenate the first and last entries, add the result to the current concatenation value, 
+and then remove these two entries.
+2. If there is only one entry left, add its value to the concatenation value and remove it from the array.
+3. Return the final concatenation value after all entries have been processed.
+"""
+# implementing the solution using Queue
+from collections import deque
+def find_the_log_conc_val(logs):
+    total = 0
+    n = len(logs)
+    if n == 0:
+        return total
+    if n == 1:
+        return logs[0]
+    q = deque(logs)
+    while len(q)>=2:
+        left = q.popleft()
+        right = q.pop()
+        concatenated = int(str(left) + str(right))
+        total += concatenated
+        if len(q) == 1:
+            total += q.pop()
+    return total
+
+
+print("--------Problem 4 (Queue)---------")
+print(find_the_log_conc_val([7, 52, 2, 4])) 
+print(find_the_log_conc_val([5, 14, 13, 8, 12])) 
+print(find_the_log_conc_val([1]))
+
+# implementing the solution using Two Pointer Approach
+def find_the_log_conc_val(logs):
+    total = 0
+    left = 0
+    right = len(logs) - 1
+    while left < right:
+        total += int(str(logs[left]) + str(logs[right]))
+        left += 1
+        right -= 1
+    if left == right:
+        total += logs[left]
+    return total
+
+
+print("--------Problem 4 (Two Pointer Approach)---------")
+print(find_the_log_conc_val([7, 52, 2, 4])) 
+print(find_the_log_conc_val([5, 14, 13, 8, 12])) 
+print(find_the_log_conc_val([1]))
+
+"""
+Problem 5: Number of Explorers Unable to Gather Supplies
+During a global expedition, explorers must gather supplies from a limited stockpile, which includes two types of resources: 
+type 0 (e.g., food rations) and type 1 (e.g., medical kits). The explorers are lined up in a queue, 
+each with a specific preference for one of the two types of resources.
+The number of supplies in the stockpile is equal to the number of explorers. The supplies are stacked in a pile. At each step:
+If the explorer at the front of the queue prefers the resource on the top of the stack, they will take it and leave the queue.
+Otherwise, they will leave the resource and go to the end of the queue.
+This process continues until no explorer in the queue wants to take the top resource, leaving some explorers 
+unable to gather the supplies they need.
+You are given two integer arrays explorers and supplies, where supplies[i] is the type of the ith resource 
+in the stack (i = 0 is the top of the stack) and explorers[j] is the preference of the jth explorer in the initial queue 
+(j = 0 is the front of the queue). Return the number of explorers that are unable to gather their preferred supplies.
+"""
+def count_explorers(explorers, supplies):
+  pass
+
+
+print("--------Problem 5---------")
+print(count_explorers([1, 1, 0, 0], [0, 1, 0, 1]))  
+print(count_explorers([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1]))
